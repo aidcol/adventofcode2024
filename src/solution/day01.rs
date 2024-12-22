2d1c3c4e531326700a3accf7e6765e2b3d66b1dd
Aidan Colon <77650375+aidcol@users.noreply.github.com>
2024-12-22 13:52:15 -0600

    Refactor Day01 methods as module-level functions

--- src/solution/day01.rs
+++ src/solution/day01.rs
@@ -1,85 +1,83 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-pub struct Day01;
+fn parse_input(path: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut list1 = vec![];
+    let mut list2 = vec![];
 
-impl Day01 {
-    fn parse_input(path: &str) -> (Vec<i32>, Vec<i32>) {
-        let mut list1 = vec![];
-        let mut list2 = vec![];
-    
-        let file = File::open(path).expect("Cannot open file");
-        let reader = BufReader::new(file);
-    
-        for line in reader.lines() {
-            let line = line.unwrap();
-            let mut items = line.split_whitespace();
-    
-            let num1: i32 = items
-                .next()
-                .unwrap()
-                .parse()
-                .expect("Cannot parse number");
-    
-            let num2: i32 = items
-                .next()
-                .unwrap()
-                .parse()
-                .expect("Cannot parse number");
-    
-            list1.push(num1);
-            list2.push(num2);
-        }
-    
-        (list1, list2)
+    let file = File::open(path).expect("Cannot open file");
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut items = line.split_whitespace();
+
+        let num1: i32 = items
+            .next()
+            .unwrap()
+            .parse()
+            .expect("Cannot parse number");
+
+        let num2: i32 = items
+            .next()
+            .unwrap()
+            .parse()
+            .expect("Cannot parse number");
+
+        list1.push(num1);
+        list2.push(num2);
     }
-    
-    fn find_distance(list1: &Vec<i32>, list2: &Vec<i32>) -> String {
-        let mut list1_clone = list1.clone();
-        let mut list2_clone = list2.clone();
-        
-        list1_clone.sort();
-        list2_clone.sort();
-    
-        let distance: i32 = std::iter::zip(list1_clone, list2_clone)
-            .map(|(a, b)| (a - b).abs())
-            .sum();
-    
-        distance.to_string()
-    }
-    
-    fn find_similarity(list1: &Vec<i32>, list2: &Vec<i32>) -> String {
-        let similarity: i32 = list1
-            .iter()
-            .map(|num| {
-                num * list2
-                    .iter()
-                    .filter(|x| &num == x)
-                    .count() as i32
-            })
-            .sum();
-    
-        similarity.to_string()
-    }
+
+    (list1, list2)
 }
+
+fn find_distance(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    let mut list1_clone = list1.clone();
+    let mut list2_clone = list2.clone();
+    
+    list1_clone.sort();
+    list2_clone.sort();
+
+    let distance: i32 = std::iter::zip(list1_clone, list2_clone)
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+
+    distance
+}
+
+fn find_similarity(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    let similarity = list1
+        .iter()
+        .map(|num| {
+            num * list2
+                .iter()
+                .filter(|x| &num == x)
+                .count() as i32
+        })
+        .sum();
+
+    similarity
+}
+
+pub struct Day01;
 
 impl super::Solution for Day01 {
     fn get_part_one(&self) -> String {
         let path = "src/input/day01.txt";
-        let (mut list1, mut list2) = Day01::parse_input(path);
+        let (mut list1, mut list2) = parse_input(path);
     
         list1.sort();
         list2.sort();
     
-        let distance = Day01::find_distance(&list1, &list2);
-        distance
+        let distance = find_distance(&list1, &list2);
+        distance.to_string()
     }
 
     fn get_part_two(&self) -> String {
         let path = "src/input/day01.txt";
-        let (list1, list2) = Day01::parse_input(path);
-        let similarity = Day01::find_similarity(&list1, &list2);
-        similarity
+        let (list1, list2) = parse_input(path);
+        let similarity = find_similarity(&list1, &list2);
+        similarity.to_string()
     }
 }
 
--- src/solution/day01/tests.rs
+++ src/solution/day01/tests.rs
@@ -6,21 +6,21 @@ fn test_parse_input() {
     let list1 = vec![3, 4, 2, 1, 3, 3];
     let list2 = vec![4, 3, 5, 3, 9, 3];
     let expected = (list1, list2);
-    assert_eq!(Day01::parse_input(path), expected);
+    assert_eq!(parse_input(path), expected);
 }
 
 #[test]
 fn test_find_distance() {
     let list1 = vec![3, 4, 2, 1, 3, 3];
     let list2 = vec![4, 3, 5, 3, 9, 3];
-    let expected = "11";
-    assert_eq!(Day01::find_distance(&list1, &list2), expected);
+    let expected = 11;
+    assert_eq!(find_distance(&list1, &list2), expected);
 }
 
 #[test]
 fn test_find_similarity() {
     let list1 = vec![3, 4, 2, 1, 3, 3];
     let list2 = vec![4, 3, 5, 3, 9, 3];
-    let expected = "31";
-    assert_eq!(Day01::find_similarity(&list1, &list2), expected);
+    let expected = 31;
+    assert_eq!(find_similarity(&list1, &list2), expected);
 }
